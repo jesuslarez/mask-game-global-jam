44d0d95b235dd3fe8145628e6f67a04e3f07d06b
JesusLarez <jesuslarez4@gmail.com>
2026-01-31 10:42:13 +0000

    escenas iniciales y sceneController y carpetas

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+public class SceneController : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+            StartGame();
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+            EndGame();
+    }
+
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+        Time.timeScale = 1;
+    }
+
+    public void GoToOptions()
+    {
+        SceneManager.LoadScene("OptionsScene");
+    }
+
+    public void GoToCredits()
+    {
+        SceneManager.LoadScene("CreditsScene");
+    }
+
+    public void EndGame()
+    {
+
+        Application.Quit();
+    }
+
+    public void GoToStart()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+}
